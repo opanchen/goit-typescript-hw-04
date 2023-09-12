25a625d0778cbc17d5b848d16e825a7157a21098
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-09-12 23:41:20 +0200

    Update 1.tsx
    - defined right type for options-object using class IntersectionObserverInit (watch Intersection Observer docs)

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -6,17 +6,13 @@ type Props = {
   onContentEndVisible: () => void;
 };
 
-type ObserverOptions = Pick<IntersectionObserver, "root" | "rootMargin"> & {
-  threshold: number;
-}; // повністю використати клас IntersectionObserver для типізації не вдається, бо він містить властивість thresholds: number[], а обʼєкт налаштувань очікує властивість threshold: number (тобто елемент масиву), тому використовуємо збірний тип...
-
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
   const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options: ObserverOptions = {
+    const options: IntersectionObserverInit = {
       rootMargin: "0px",
       threshold: 1.0,
       root: null,
